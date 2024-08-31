1ee98440def3f979ec275f0cc394bba8602fcaf6
Yuki-not-cutie <yuuki.not.cutie@gmail.com>
2024-09-01 00:34:12 +0100

    upload

---
+++ xxxxx/node_2d.gd
@@ -0,0 +1,47 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_5_pressed():
+	$AudioStreamPlayer5.play()
+
+
+func _on_button_6_pressed():
+	$AudioStreamPlayer6.play()
+
+
+func _on_button_7_pressed():
+	$AudioStreamPlayer7.play()
+
+
+func _on_button_8_pressed():
+	$AudioStreamPlayer8.play()
+
+
+func _on_button_9_pressed():
+	$AudioStreamPlayer9.play()
+
+
+func _on_button_1_pressed():
+	$AudioStreamPlayer1.play()
+
+
+func _on_button_2_pressed():
+	$AudioStreamPlayer2.play()
+
+
+func _on_button_3_pressed():
+	$AudioStreamPlayer3.play()
+
+
+func _on_button_4_pressed():
+	$AudioStreamPlayer4.play()
